7a9a8a6cf15b3dd6efc12115d11f79f3c7ab2f12
Valeentin87 <valeentin87@list.ru>
2023-09-29 16:13:20 +0300

    протестировал 4 задачу на корректную и некорректную строку

--- src/main/java/org/example/Example4.java
+++ src/main/java/org/example/Example4.java
@@ -7,11 +7,24 @@ import java.util.Scanner;
 
 public class Example4 {
     public static void main(String[] args) {
-        String data;
         Scanner sc = new Scanner(System.in);
-
-        data = sc.nextLine();
-
+        System.out.println("Введите значение: ");
+        String message;
+        try {
+            message = sc.nextLine();
+            stringInput(message);
+            System.out.println("Введенная строка корректна!!!");
+        } catch (NullStringExeption e) {
+            System.out.println("Некорректный ввод: " + e.getMessage());
+        }
+        finally {
+            sc.close();
+        }
     }
 
+    public static void stringInput(String input) throws NullStringExeption {
+        if (input == null || input.isEmpty()) {
+            throw new NullStringExeption("Входная строка не может быть null или пустой");
+        }
+    }
 }
