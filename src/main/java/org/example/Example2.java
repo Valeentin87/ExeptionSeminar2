1ef3775fe1e95343ad6a173c1ccc4beb26798382
Valeentin87 <valeentin87@list.ru>
2023-09-29 15:34:13 +0300

    добавил обработку ошибки выхода за пределы массива

--- src/main/java/org/example/Example2.java
+++ src/main/java/org/example/Example2.java
@@ -6,11 +6,13 @@ public class Example2 {
     public static void main(String[] args) {
         int[] intArray = new int[10];
         try {
-            int d = 0;
-            double catchedRes1 = intArray[8] / d;
+            int d = 1;
+            double catchedRes1 = intArray[11] / d;
             System.out.println("catchedRes1 = " + catchedRes1);
         } catch (ArithmeticException e) {
             System.out.println("Catching exception: " + e);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Unsupported index number of intArray!!! ");
         }
 
     }
